9b33a0e70489db608ceb8a850bf59bf69e7448e1
Maksym Pakula <gargantadoto@gmail.com>
2026-04-25 22:05:20 +0300

    Add User, SocialAccount, City, Profile models

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,124 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+)
+from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext as _
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for a User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    first_name = None
+    last_name = None
+    email = models.EmailField(_("email address"), unique=True)
+    is_verified = models.BooleanField(_("verified"), default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class SocialAccount(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="social_account"
+    )
+    provider = models.CharField(max_length=50)
+    uid = models.CharField(max_length=255)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["uid", "provider"],
+                name="unique_social_account",
+            )
+        ]
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+
+
+class GenderChoices(models.TextChoices):
+    MALE = "Male"
+    FEMALE = "Female"
+    OTHER = "Other"
+
+
+def upload_profile_picture(instance, filename):
+    """Upload a post picture."""
+    ext = filename.split(".")[-1]
+    filename = f"{slugify(instance.username)}-{uuid.uuid4()}.{ext}"
+
+    return os.path.join("uploads/posts/", filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    username = models.CharField(max_length=60, unique=True)
+    first_name = models.CharField(max_length=60, null=True, blank=True)
+    last_name = models.CharField(max_length=60, null=True, blank=True)
+    bio = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
+    social_media_url = models.URLField(blank=True, null=True)
+    avatar = models.ImageField(upload_to=upload_profile_picture, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    gender = models.CharField(max_length=20, choices=GenderChoices.choices)
+
+    def __str__(self):
+        return self.username
