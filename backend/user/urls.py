9728d439edc3dcc1b94fa9a3913ba749211790ae
Maksym Pakula <gargantadoto@gmail.com>
2026-04-26 05:22:04 +0300

    Add JWT and Google authentication endpoints

---
+++ backend/user/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from user.views import google_auth
+
+
+urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/google/", google_auth, name="google"),
+]
+
+app_name = "user"
