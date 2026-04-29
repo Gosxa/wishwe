b4d03a0137cef1f009f511dec80e4d2fbd8322ec
Maksym Pakula <gargantadoto@gmail.com>
2026-04-29 21:45:50 +0300

    Add real email sending

--- backend/wishwe_api/settings.py
+++ backend/wishwe_api/settings.py
@@ -144,4 +144,11 @@ SIMPLE_JWT = {
    "ROTATE_REFRESH_TOKENS": True
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
