13e4ce2dfc8a94642ee27ccc08898f4cf4f72f17
Maksym Pakula <gargantadoto@gmail.com>
2026-05-06 04:45:01 +0300

    Add tests for friendship

--- backend/wishwe_api/settings.py
+++ backend/wishwe_api/settings.py
@@ -20,19 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "wishwe.online",
+    "www.wishwe.online",
+]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
