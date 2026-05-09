add8a3d0862943c8bc7b88a2f96f7e78a00836ad
Maksym Pakula <gargantadoto@gmail.com>
2026-05-10 02:43:51 +0300

    Add event Category model

--- backend/event/models.py
+++ backend/event/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
