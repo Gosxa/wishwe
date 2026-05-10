cfd8c1a114cd2e02728a6204ec46b5712ce15087
Maksym Pakula <gargantadoto@gmail.com>
2026-05-10 04:43:11 +0300

    Add Wish and Plan write serializers

--- backend/event/serializers.py
+++ backend/event/serializers.py
@@ -86,3 +86,131 @@ class EventSerializer(serializers.ModelSerializer):
             "is_expired",
             "available_spots",
         )
+
+
+class WishWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = (
+            "id",
+            "category",
+            "title",
+            "description",
+            "cover_image",
+            "location",
+            "external_link",
+            "timeframe_text",
+            "min_participants",
+            "expires_at",
+        )
+
+    def validate(self, attrs):
+        instance = self.instance
+
+        if (
+            instance
+            and instance.status in (
+                EventStatus.COMPLETED,
+                EventStatus.CANCELLED,
+            )
+        ):
+            raise serializers.ValidationError(
+                "Completed or cancelled events cannot be edited."
+            )
+
+        timeframe_text = attrs.get(
+            "timeframe_text",
+            instance.timeframe_text if instance else None,
+        )
+
+        if not timeframe_text:
+            raise serializers.ValidationError({
+                "timeframe_text":
+                    "Wish events require timeframe_text."
+            })
+
+        return attrs
+
+    def create(self, validated_data):
+        validated_data["creator"] = self.context["request"].user
+        validated_data["event_type"] = EventType.WISH
+
+        return super().create(validated_data)
+
+
+class PlanWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = (
+            "id",
+            "category",
+            "title",
+            "description",
+            "cover_image",
+            "location",
+            "external_link",
+            "event_date",
+            "event_time",
+            "min_participants",
+            "max_participants",
+            "expires_at",
+        )
+
+    def validate(self, attrs):
+        instance = self.instance
+        if (
+            instance
+            and instance.status in (
+                EventStatus.COMPLETED,
+                EventStatus.CANCELLED,
+            )
+        ):
+            raise serializers.ValidationError(
+                "Completed or cancelled events cannot be edited."
+            )
+
+        min_participants = attrs.get(
+            "min_participants",
+            instance.min_participants if instance else None,
+        )
+
+        max_participants = attrs.get(
+            "max_participants",
+            instance.max_participants if instance else None,
+        )
+
+        if max_participants < min_participants:
+            raise serializers.ValidationError({
+                "max_participants":
+                    "max_participants cannot be less than min_participants."
+            })
+
+        event_date = attrs.get(
+            "event_date",
+            instance.event_date if instance else None,
+        )
+
+        event_time = attrs.get(
+            "event_time",
+            instance.event_time if instance else None,
+        )
+
+        if not event_date:
+            raise serializers.ValidationError({
+                "event_date":
+                    "Plan events require event_date."
+            })
+
+        if not event_time:
+            raise serializers.ValidationError({
+                "event_time":
+                    "Plan events require event_time."
+            })
+
+        return attrs
+
+    def create(self, validated_data):
+        validated_data["creator"] = self.context["request"].user
+        validated_data["event_type"] = EventType.PLAN
+
+        return super().create(validated_data)
