bc532b54c9ca6fb79748bf03b8ecd8175c557a1e
Maksym Pakula <gargantadoto@gmail.com>
2026-05-12 04:40:34 +0300

    Add initial feed, create wish & plan endpoints

--- backend/event/urls.py
+++ backend/event/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from event.views import CategoryViewSet
-
+from event.views import CategoryViewSet, EventViewSet
 
 router = DefaultRouter()
 router.register("category", CategoryViewSet)
+router.register("events", EventViewSet, basename="event")
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/event/views.py
+++ backend/event/views.py
@@ -1,9 +1,100 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from event.models import Category
-from event.serializers import CategorySerializer
+from event.models import (
+    Category,
+    Event,
+    EventStatus,
+    EventType
+)
+from event.serializers import (
+    CategorySerializer,
+    EventSerializer,
+    PlanWriteSerializer,
+    WishWriteSerializer
+)
+from event.services.event_service import EventService
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        if self.action in ("list", "retrieve"):
+            return [permissions.AllowAny()]
+
+        return [permissions.IsAdminUser()]
+
+
+class EventViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = EventSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    _visible_users_ids = None
+
+    def get_visible_users_ids(self):
+        if self._visible_users_ids is None:
+            self._visible_users_ids = (
+                EventService.get_visible_users_ids(
+                    self.request.user
+                )
+            )
+
+        return self._visible_users_ids
+
+    def get_queryset(self):
+        visible_users_ids = self.get_visible_users_ids()
+
+        queryset = Event.objects.filter(
+            creator_id__in=visible_users_ids,
+            status=EventStatus.ACTIVE,
+        ).select_related(
+            "creator__profile",
+            "category",
+        ).order_by("-created_at")
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "create_wish":
+            return WishWriteSerializer
+
+        if self.action == "create_plan":
+            return PlanWriteSerializer
+
+        return EventSerializer
+
+    @action(detail=False, methods=["post"])
+    def create_wish(self, request):
+        serializer = self.get_serializer(
+            data=request.data
+        )
+        serializer.is_valid(raise_exception=True)
+
+        event = EventService.create_wish(
+            creator=request.user,
+            validated_data=serializer.validated_data,
+        )
+
+        return Response(
+            EventSerializer(event).data,
+            status=status.HTTP_201_CREATED,
+        )
+
+    @action(detail=False, methods=["post"])
+    def create_plan(self, request):
+        serializer = self.get_serializer(
+            data=request.data
+        )
+        serializer.is_valid(raise_exception=True)
+
+        event = EventService.create_plan(
+            creator=request.user,
+            validated_data=serializer.validated_data,
+        )
+
+        return Response(
+            EventSerializer(event).data,
+            status=status.HTTP_201_CREATED,
+        )
