6091455903a560585df9fe1e982a11c6fe4f4de2
Bespalov Tymofii <drdakkadakka@gmail.com>
2026-05-19 15:37:28 +0200

    add favicon

--- frontend/src/client_pages/onboard/OnBoard.tsx
+++ frontend/src/client_pages/onboard/OnBoard.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import s from './onBoard.module.scss';
+
 export const OnBoard = () => {
   return (
-    <div>
-      <picture></picture>
-      <main></main>
-    </div>
+    <main className={s.container}>
+      <picture>
+        <img src="/onboard_image.jpg" alt="" />
+      </picture>
+      <div className={s.inner}></div>
+    </main>
   );
 };
