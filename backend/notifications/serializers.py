db50653813305b165c66745d920abfd33c0f6532
Maksym Pakula <gargantadoto@gmail.com>
2026-05-20 18:14:56 +0300

    add notification serializer

---
+++ backend/notifications/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from notifications.models import Notification
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    recipient = serializers.CharField(
+        source="recipient.profile.username",
+        read_only=True
+    )
+    creator = serializers.CharField(
+        source="creator.profile.username",
+        read_only=True
+    )
+
+    class Meta:
+        model = Notification
+        fields = (
+            "id",
+            "title",
+            "message",
+            "type",
+            "recipient",
+            "creator",
+            "related_object_type",
+            "related_object_id",
+            "is_read"
+        )
+        read_only_fields = fields
