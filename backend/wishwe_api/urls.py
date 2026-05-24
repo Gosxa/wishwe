aea2ef911173e4286a3a3e102e1c1a8c241e0a9f
Maksym Pakula <gargantadoto@gmail.com>
2026-05-24 16:51:33 +0300

    Add username availability check endpoint

--- backend/wishwe_api/urls.py
+++ backend/wishwe_api/urls.py
@@ -2,8 +2,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from user.views import health_check
-
+from user.views import health_check, UsernameAvailabilityAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,6 +10,11 @@ urlpatterns = [
     path("api/event/", include("event.urls", namespace="events")),
     path("api/notifications/", include("notifications.urls", namespace="notifications")),
     path("api/health/", health_check, name="health_check"),
+    path(
+        "api/username-check/",
+        UsernameAvailabilityAPIView.as_view(),
+        name="username_availability"
+    ),
     path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
     path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ] + debug_toolbar_urls()
