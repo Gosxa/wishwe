e828cc629410c7ef81da9746d815b9810dea4cd6
Bespalov Tymofii <drdakkadakka@gmail.com>
2026-05-31 17:06:01 +0300

    Features/onboarding (#37)
    * add base next env, libs, constants, fonts
    
    * add favicon
    
    * add register screen, login screen, button mixins, shared input component
    
    * add avatar crop, add config + map pattern for personal form
    
    * added login screens
    
    * recovering
    
    * prepull
    
    * conflict solved
    
    * pre-pull
    
    * processing form
    
    * pre-userDataForm
    
    * conflict fix
    
    * add timer && resend on email code input
    
    * switch imperative track move to stack declarative logic
    
    * onboarding screen v1.1
    
    * conflicts
    
    * fix merge conflict
    
    Removed duplicate logger initialization and resolved merge conflict.
    
    * Remove unnecessary blank line in views.py
    
    * deploy fixes
    
    * pl fixes
    
    ---------
    
    Co-authored-by: Gosxa <ihorkondrashov@gmail.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from 'next';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(__dirname, '../.env') });
 
 const nextConfig: NextConfig = {
   output: 'standalone',
   reactCompiler: true,
+  env: {
+    NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID:
+      process.env.GOOGLE_OAUTH_CLIENT_ID ?? '',
+  },
   async rewrites() {
     return [
       {
