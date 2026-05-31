900324467d0fac9eb17ceaaa4b4ec0ab0c9069a4
Maksym Pakula <114878517+StrawHato@users.noreply.github.com>
2026-05-31 13:55:16 +0300

    Added beautiful email fill (#35)
    * Add username availability check endpoint
    
    * add beautiful email fill
    
    ---------
    
    Co-authored-by: Maksym Pakula <gargantadoto@gmail.com>

--- backend/wishwe_api/settings.py
+++ backend/wishwe_api/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = "wishwe_api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
