6cc956e3388bd4ad037185cd7b8adc55a2e2c583
Maksym Pakula <gargantadoto@gmail.com>
2026-07-28 18:52:12 +0200

    add scheduled task send_friend_request_reminder_email
    sends email if receiver didn't accept friend request in 30 minutes

--- backend/user/tasks.py
+++ backend/user/tasks.py
@@ -1,7 +1,10 @@
 from celery import shared_task
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.utils import timezone
 
-from user.models import FriendInvite
+from user.models import FriendInvite, Friendship, FriendshipStatus
 
 
 @shared_task
@@ -9,3 +12,45 @@ def delete_expired_invites():
     FriendInvite.objects.filter(
         expires_at__lt=timezone.now()
     ).delete()
+
+
+@shared_task(
+    bind=True,
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    retry_kwargs={"max_retries": 3},
+)
+def send_friend_request_reminder_email(self, friendship_id: int) -> None:
+    try:
+        friendship = Friendship.objects.select_related(
+            "sender",
+            "receiver",
+        ).get(id=friendship_id)
+    except Friendship.DoesNotExist:
+        return
+
+    if friendship.status != FriendshipStatus.PENDING:
+        return
+
+    receiver = friendship.receiver
+
+    context = {
+        "name": receiver.profile.username,
+        "sender_name": friendship.sender.profile.username,
+        "friends_url": f"{settings.FRONTEND_URL}/friends",
+    }
+
+    html_content = render_to_string(
+        "emails/friend_request.html",
+        context,
+    )
+
+    email = EmailMultiAlternatives(
+        subject="You have a pending friend request",
+        body="",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[receiver.email],
+    )
+
+    email.attach_alternative(html_content, "text/html")
+    email.send()
