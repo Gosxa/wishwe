a2891648cd969f7e755818d84ad26fbba68d55a8
Mykola Sheiko <nyr3mvneov@gmail.com>
2026-08-10 15:41:58 +0300

    remove unused padding adjustment in useBodyScrollLock

--- frontend/src/features/useBodyScrollLock/useBodyScrollLock.ts
+++ frontend/src/features/useBodyScrollLock/useBodyScrollLock.ts
@@ -4,22 +4,13 @@ import { useEffect } from 'react';
 
 let lockCount = 0;
 let restoreOverflow = '';
-let restorePaddingRight = '';
 
 export const useBodyScrollLock = () => {
   useEffect(() => {
     if (lockCount === 0) {
       restoreOverflow = document.body.style.overflow;
-      restorePaddingRight = document.body.style.paddingRight;
-
-      const scrollbarWidth =
-        window.innerWidth - document.documentElement.clientWidth;
 
       document.body.style.overflow = 'hidden';
-
-      if (scrollbarWidth > 0) {
-        document.body.style.paddingRight = `${scrollbarWidth}px`;
-      }
     }
 
     lockCount += 1;
@@ -29,7 +20,6 @@ export const useBodyScrollLock = () => {
 
       if (lockCount === 0) {
         document.body.style.overflow = restoreOverflow;
-        document.body.style.paddingRight = restorePaddingRight;
       }
     };
   }, []);
